9e56450196f8eb8e989a85b6b3da59bdb752ea82
Manisha-singh <kajalsinghks607737@gmail.com>
2022-01-04 22:00:36 +0530

    question analysis related api and frontend

--- src/main/java/com/letsTest/controller/StudentController.java
+++ src/main/java/com/letsTest/controller/StudentController.java
@@ -1,7 +1,29 @@
 package com.letsTest.controller;
 
+import com.letsTest.dto.ResultDto;
+import com.letsTest.service.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/results")
 public class StudentController {
+
+    @Autowired
+    ResultService resultService;
+
+    @RequestMapping(value="/save",method = RequestMethod.POST)
+    @ResponseBody
+    public String SaveResultMarks(@RequestBody ResultDto resultDto){
+        return resultService.SaveResultMarks(resultDto);
+    }
+
+    @RequestMapping(value="/get",method = RequestMethod.GET)
+    @ResponseBody
+    public List<ResultDto> getResultMarks(@RequestParam Long quizId, @RequestParam Long createdById){
+        return resultService.getResultMarks(quizId,createdById);
+    }
 }
